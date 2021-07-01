3f47acde6a44f0cce21d9c506cb6455a26ce1fe2
Denis Matvienko <grabsomebuds27@gmail.com>
2021-07-01 10:18:45 +0300

    Initial migrations

---
+++ ImageUploader/ImageEditor/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.db import models
+
+from django.db import models
+
+
+class Img(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    width = models.PositiveIntegerField(blank=True)
+    height = models.PositiveIntegerField(blank=True)
+    image = models.ImageField(upload_to="img/", blank=False, null=False)
+
+    def __str__(self):
+        return self.name
--- ImageUploader/ImageUploader/settings.py
+++ ImageUploader/ImageUploader/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ImageEditor',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +54,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ImageUploader.urls'
 
+DIR_FOR_TEMPLATES = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [DIR_FOR_TEMPLATES],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +127,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
