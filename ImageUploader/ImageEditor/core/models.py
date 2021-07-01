3153822705543639cbceee2252541e3f2d961375
Denis Matvienko <grabsomebuds27@gmail.com>
2021-07-01 10:22:39 +0300

    Models modified

--- ImageUploader/ImageEditor/core/models.py
+++ ImageUploader/ImageEditor/core/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-from django.db import models
-
-from django.db import models
-
 
 class Img(models.Model):
     name = models.CharField(max_length=200, blank=False, null=False)
